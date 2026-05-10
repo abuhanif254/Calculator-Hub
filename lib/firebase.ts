835bdc0187736242058dfcb3a41de017ae41de1b
md abu hanif mia <mohammadbitullah@gmail.com>
2026-05-11 05:51:15 +0600

    solve deployment error

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,8 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 import firebaseConfig from "../firebase-applet-config.json";
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Use long polling to prevent Next.js build (SSR/SSG) from hanging due to gRPC connection issues
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true
+}, firebaseConfig.firestoreDatabaseId);
+
 export const auth = getAuth(app);
