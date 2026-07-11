3c904373ee058685f383fa300d1d0ca727b37172
md abu hanif mia <mohammadbitullah@gmail.com>
2026-07-11 11:46:00 +0600

    Clean up unused files to fix Vercel build

--- _trash/test-resolver.ts
+++
@@ -1,3 +0,0 @@
-import { resolveHref } from './lib/utils/linkResolver';
-console.log('AI Image Upscaler:', resolveHref('AI Image Upscaler'));
-console.log('Blur Faces in Image:', resolveHref('Blur Faces in Image'));
